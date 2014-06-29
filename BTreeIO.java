a05bf80399b2da2e1599a1440dff9e7f434da6ff
AyrtonStout <AyrtonStout@u.boisestate.edu>
2014-06-28 20:23:48 -0600

    This file is (nearly) complete and functional, but is untested

--- BTreeIO.java
+++ BTreeIO.java
@@ -5,17 +5,27 @@ import java.util.Scanner;
 
 /**
  * @author astout
- * Parses the gene bank file
- * Returns the next long to be added to the BTree
- * 
- * --Maybe does other stuff?
+ * The BTreeIO class will parse the genebank file passed into its constructor and discard all non-gene data. It will then grab the first gene sequence, convert it into 
+ * a long, and hold onto it until it is asked to return it through the getNextLong() method. Any time the getNextLong() method is called, this class will automatically 
+ * grab the next valid gene sequence. It is recommended to always check to see if BTreeIO hasNext() before calling getNextLong(), or it will return 0s.
  */
 public class BTreeIO {
 
 	private String parsedInput;
-	
-	public BTreeIO(String fileName)	{
-	
+	private int fileIndex = 0;
+	private long nextLong;
+	private int sequenceLength;
+
+	/**
+	 * BTreeIO when created will parse the given genebank file and discard all non-gene data that it contains. 
+	 * It will then hold onto the next gene sequence to be returned.
+	 * 
+	 * @param fileName The file name of the gene bank file to be parsed.
+	 * @param sequenceLength The length of the gene sequences to be parsed from the gene bank file.
+	 */
+	public BTreeIO(String fileName, int sequenceLength)	{
+		this.sequenceLength = sequenceLength;
+
 		StringBuilder builder = new StringBuilder();
 		Scanner scan = null;
 		try {
@@ -23,30 +33,102 @@ public class BTreeIO {
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
-		
+
 		String input = "";
 		while (input.compareTo("ORIGIN") != 0)	{
 			input = scan.nextLine().trim();
 		}
-		
+
+		//TODO this currently will only parse the first series of genes from a given file. It will not continue after it has encountered its first '/'
 		while (scan.hasNext())	{
-			char next = scan.next().charAt(0);
-//			if (next == '/')	{
-//				break;
-//			}
-//			if (next > '9')	{
-				builder.append(next);
-//			}
+			String line = scan.nextLine();
+			for (int i = 0; i < line.length(); i++)	{
+				char next = line.charAt(i);
+				if (next == '/')
+					break;
+				if (next > '9')
+					builder.append(line.charAt(next));
+			}
 		}
-		
+
 		parsedInput = builder.toString();
-		System.out.println(parsedInput);
-//		for (int i = 0; i < parsedInput.length(); i++)	{
-//			System.out.print(parsedInput.charAt(i));
-//			if (i % 10 == 0)	{
-//				System.out.print("\n");
-//			}
-//		}
+
+		nextLong = findNextLong();
+
 	}
 	
+	/**
+	 * Returns 1 + the next gene sequence as a long data type, or 0 if there is no more data.
+	 *  
+	 * @return The gene sequence plus a leading 1 to prevent loss of leading zeroes. Example, the gene sequence "AGT" would be returned as "1001011", or "CA" would be
+	 * returned as "1000".
+	 */
+	public long getNextLong()	{
+		long rval = nextLong;
+		nextLong = findNextLong();
+		return rval;
+	}
+	
+	/**
+	 * Returns whether or not there is more DNA data to be returned.
+	 * 
+	 * @return This will return true if there is another full length valid gene sequence.
+	 */
+	public boolean hasNext()	{
+		if (nextLong == 0)
+			return false;
+		return true;
+	}
+
+	/**
+	 * This will attempt to locate the next long inside the gene sequence. Any invalid gene sequences are searched through and discarded.
+	 * 
+	 * @return The next valid gene sequence, or 0 if there are no more valid gene sequences.
+	 */
+	private long findNextLong()	{
+		long rval = 0;
+		while (rval == 0)	{
+			if (sequenceLength + fileIndex > parsedInput.length())	{
+				return 0;
+			}
+			rval = stringToLong(parsedInput.substring(fileIndex, fileIndex + sequenceLength));
+			fileIndex++;
+		}
+		return rval;
+	}
+
+	/**
+	 * Converts a string into a long where A = 00, C = 01, G = 10, and T = 11. If at any time, an invalid character is encountered (namely the 'n' character), this method
+	 * will return 0.
+	 * 
+	 * @param input The gene sequence to be converted.
+	 * @return The gene sequence converted into binary, or 0 if the gene sequence was invalid. 
+	 */
+	private long stringToLong(String input)	{
+		long output = 1;
+		for (int i = 0; i < input.length(); i++)	{
+			switch(input.charAt(i))	{
+			case 'a':
+				output *= 100;
+				break;
+			case 'c':
+				output *= 100;
+				output += 1;
+				break;
+			case 'g':
+				output *= 100;
+				output += 10;
+				break;
+			case 't':
+				output *= 100;
+				output += 11;
+				break;
+			default:
+				return 0;
+				//TODO potential optimization of this to have it skip ahead when encountering an invalid character instead of parsing it numerous times. Very minor
+			}
+		}
+		return output;
+	}
+
 }
